b88890fa3034bc0c76f27864f22f466f5d23005f
Andrea <andrea.giachetto1992@gmail.com>
2025-03-11 17:06:11 +0100

    Miglioramento preview e struttura dati

--- studio/schemas/documents/collections/news.js
+++ studio/schemas/documents/collections/news.js
@@ -1,7 +1,9 @@
+import { slugify, validateSlug } from '@/utils/helperFunctions.js'
 import { BiFile } from 'react-icons/bi/'
 import pageFields from '../pageComponents/pageFields'
 import groups from '../groups'
 import { defineField } from 'sanity'
+import pageComponents from '../pageComponents'
 
 export default {
   title: 'News',
@@ -17,6 +19,64 @@ export default {
     }
   ],
   fields: [
+
+    {
+      title: 'Titolo del contenuto',
+      name: 'title',
+      type: 'text',
+      rows: 2,
+      validation: (Rule) => Rule.required(),
+      group: 'content',
+    },
+    defineField({
+      title: 'Sottotitolo',
+      description: 'Inserisci il sottotitolo dell\'articolo.',
+      name: 'subtitle',
+      type: 'text',
+      rows: 2,
+      validation: Rule => Rule.required(),
+      group: 'content',
+    }),
+    {
+      title: 'Slug',
+      description: 'URL univoco per questa pagina. Se lasciato vuoto, verrà generato automaticamente.',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        slugify: slugify,
+      },
+      validation: validateSlug,
+      group: 'settings',
+    },
+    {
+      title: 'Immagine',
+      description: 'Attenzione: questa immagine verrà usata come anteprima della notizia, come testata della pagina dettaglio e come Open Graph Image.',
+      name: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      group: 'settings',
+    },
+    defineField({
+      title: 'Contenuto della pagina corrente',
+      name: 'content',
+      type: 'object',
+      group: 'content',
+      fields: [
+        defineField({
+          title: 'Componenti',
+          name: 'components',
+          description:
+            'Crea il contenuto di questa pagina. Aggiungi nuove sezioni per visualizzare del contenuto nella pagina.',
+          type: 'array',
+          // Note: if you want to add manually sections
+          // of: [{type: 'your-section'}, ...]
+          of: pageComponents.map((section) => ({ type: section.name })),
+        }),
+      ],
+    }),
     {
       title: 'In evidenza',
       description: 'Seleziona per mettere in evidenza la news.',
@@ -40,15 +100,18 @@ export default {
     }),
     defineField({
       title: 'Minuti di lettura',
-      description: 'Inserisci i minuti di lettura necessari,.',
+      description: 'Inserisci i minuti di lettura necessari.',
       name: 'readingTime',
       type: 'number',
       validation: Rule => Rule.required(),
       group: 'settings',
     }),
-
-    ...pageFields.map(field => field),
-    // field true / false "in evidenza"
+    {
+      title: 'SEO',
+      name: 'seo',
+      type: 'seo',
+      group: 'seo',
+    },
 
     // field reference to type: 'tags', 'temi', 'quartiere'
     {
@@ -66,7 +129,6 @@ export default {
       type: 'array',
       of: [
         { type: 'reference', to: [{ type: 'tag' }] }],
-      validation: Rule => Rule.required().min(1),
       group: 'assegnazioni'
     },
 
@@ -76,7 +138,6 @@ export default {
       type: 'array',
       of: [
         { type: 'reference', to: [{ type: 'quartiere' }] }],
-      validation: Rule => Rule.required().min(1),
       group: 'assegnazioni'
     },
   ],
